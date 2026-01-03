01e70e5b751c697b92e168ffd67e63aa6a84c0a8
vinudasenith <ha.vinudas@gmail.com>
2026-01-03 15:54:09 +0530

    feat(nav): add navigation button to LoginScreen

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+// app/index.tsx
+import { useRouter } from 'expo-router';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+export default function Home() {
+    const router = useRouter();
+
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-xl mb-4">Home Page</Text>
+
+            <TouchableOpacity
+                onPress={() => router.push('/login')}
+                className="bg-blue-600 px-6 py-3 rounded-lg"
+            >
+                <Text className="text-white">Go to Login</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
